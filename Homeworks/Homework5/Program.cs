ff4e45e6d64d030c5419c03eadd181859f212e84
T8mpest <opcache@gmail.com>
2023-06-15 18:05:09 +0300

    homework 5.1 last version

---
+++ Homeworks/Homework5/Program.cs
@@ -0,0 +1,21 @@
+﻿while (true) {
+    int[] numbers = { 1, 4, 7, 2, 8, 6, 3 };
+    Console.WriteLine("Введите число которое хотите найти");
+    int target = int.Parse(Console.ReadLine());
+    int pos = -1;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i] == target)
+        { pos = i;
+            break;
+        }
+    }
+    if (pos != -1)
+    {
+        Console.WriteLine($"{target} Найдено на позиции {pos}");
+    }
+    else
+    {
+        Console.WriteLine($"{target} Не найдено в массиве, попробуйте еще раз");
+    }
+}
