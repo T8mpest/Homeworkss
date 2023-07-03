b06c099ba2d884742083f013dfd7a8e10d64ef96
T8mpest <opcache@gmail.com>
2023-07-04 02:29:40 +0300

    done)

---
+++ Homeworks/Homework10/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    abstract class Car
+    {
+        public string Brand { get; set; }
+        public int MaxSpeed { get; set; }
+
+        public abstract void Accelerate();
+        public abstract void Brake();
+
+        public virtual void GetSpeed()
+        {
+            Console.WriteLine($"The {Brand} car is currently running at {MaxSpeed} km/h.");
+        }
+    }
+
+    class BMW : Car
+    {
+        public BMW()
+        {
+            Brand = "BMW";
+            MaxSpeed = 200;
+        }
+
+        public override void Accelerate()
+        {
+            Console.WriteLine($"The {Brand} car is accelerating.");
+            MaxSpeed += 10;
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Brand} car is braking.");
+            MaxSpeed -= 20;
+        }
+    }
+
+    class Audi : Car
+    {
+        public Audi()
+        {
+            Brand = "Audi";
+            MaxSpeed = 180;
+        }
+
+        public override void Accelerate()
+        {
+            Console.WriteLine($"The {Brand} car is accelerating.");
+            MaxSpeed += 8;
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Brand} car is braking.");
+            MaxSpeed -= 16;
+        }
+    }
+    class Mclaren : Car
+    { 
+     public Mclaren()
+        {
+            Brand = "Mclaren";
+            MaxSpeed = 260;
+        }
+        public override void Accelerate()
+        {
+            Console.WriteLine($"The {Brand} car is accelerating.");
+            MaxSpeed += 18;
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Brand} car is braking.");
+            MaxSpeed -= 30;
+        }
+    }
+}
+
+
---
+++ Homeworks/Homework10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework10
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Car bmwCar = new BMW();
+            bmwCar.GetSpeed();
+            bmwCar.Accelerate();
+            bmwCar.GetSpeed();
+            bmwCar.Brake();
+            bmwCar.GetSpeed();
+
+            Console.WriteLine();
+
+            Car audiCar = new Audi();
+            audiCar.GetSpeed();
+            audiCar.Accelerate();
+            audiCar.GetSpeed();
+            audiCar.Brake();
+            audiCar.GetSpeed();
+            
+            Console.WriteLine();
+
+            Car mclarenCar = new Mclaren();
+            mclarenCar.GetSpeed();
+            mclarenCar.Accelerate();
+            mclarenCar.GetSpeed();
+            mclarenCar.Brake();
+            mclarenCar.GetSpeed();
+        }
+    }
+}
